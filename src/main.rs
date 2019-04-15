7276a3c3a9c527283dc69e64ffb49b76ef774a5b
SungYong Jang <jsy@adain.kr>
2019-04-15 12:02:25 +0900

    12.6 complete

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,12 @@ use greprs::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments.\n{}", err);
+        eprintln!("Problem parsing arguments.\n{}", err);
         process::exit(1);
     });
 
     if let Err(e) = greprs::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 
